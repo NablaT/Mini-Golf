f09f123eb140fe390efb392639ab395b72edb2fd
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-12-13 17:46:13 +0100

    Un premier projet qui fonctionne avec angular. Pas de serveur encore :/

---
+++ DesktopApp/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desktopApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the desktopApp
+ */
+angular.module('desktopApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ DesktopApp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desktopApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the desktopApp
+ */
+angular.module('desktopApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
