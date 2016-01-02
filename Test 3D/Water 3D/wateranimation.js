2ee1086f1393c90ab47c5c41a601d837047ab814
NablaT <remi.pourtier@laposte.net>
2016-01-02 16:38:32 +0100

    Adding texture and refactoring water script

--- Test 3D/Water 3D/wateranimation.js
+++ Test 3D/Water 3D/wateranimation.js
@@ -4,13 +4,16 @@ if (BABYLON.Engine.isSupported()) {
     var engine = new BABYLON.Engine(canvas, true);
     var scene = new BABYLON.Scene(engine);
     //Initialisation camera
-    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, BABYLON.Vector3.Zero(), scene);
+    //var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, BABYLON.Vector3.Zero(), scene);
+    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10,new BABYLON.Vector3(0, 0, 0), scene);
+
+
     var sun = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(60, 100, 10), scene);
 
     camera.setPosition(new BABYLON.Vector3(-40, 40, 0));
 
     // Skybox
-    /*var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
     var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
     skyboxMaterial.backFaceCulling = false;
     skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("skybox/TropicalSunnyDay", scene);
@@ -18,11 +21,11 @@ if (BABYLON.Engine.isSupported()) {
     skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
     skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
     skybox.material = skyboxMaterial;
-    */
 
-    var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "heightMap.png", 100, 100, 100, 0, 10, scene, false);
+
+    var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "heightMap2.png", 100, 100, 100, 0, 10, scene, false);
     var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
-    groundMaterial.diffuseTexture = new BABYLON.Texture("Tree.png", scene);
+    groundMaterial.diffuseTexture = new BABYLON.Texture("grass.png", scene);
     groundMaterial.diffuseTexture.uScale = 6;
     groundMaterial.diffuseTexture.vScale = 6;
     groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
@@ -39,8 +42,9 @@ if (BABYLON.Engine.isSupported()) {
             scene.materials.push(this);
         };
 
-        WaterMaterial.prototype = Object.create(BABYLON.Material.prototype);
 
+        WaterMaterial.prototype = Object.create(BABYLON.Material.prototype);
+        WaterMaterial.bumpTexture = new BABYLON.Texture("water.png", scene);
         // Properties
         WaterMaterial.prototype.needAlphaBlending = function () {
             return false;
@@ -61,6 +65,13 @@ if (BABYLON.Engine.isSupported()) {
         WaterMaterial.prototype.dispose = function () {
             this.baseDispose();
         };
+        WaterMaterial.windForce = 45; // Represents the wind force applied on the water surface
+        WaterMaterial.waveHeight = 1.3; // Represents the height of the waves
+        WaterMaterial.bumpHeight = 0.3; // According to the bump map, represents the pertubation of reflection and refraction
+        WaterMaterial.windDirection = new BABYLON.Vector2(1.0, 1.0); // The wind direction on the water surface (on width and height)
+        WaterMaterial.waterColor = new BABYLON.Color3(0.1, 0.1, 0.6); // Represents the water color mixed with the reflected and refracted world
+        WaterMaterial.colorBlendFactor = 2.0; // Factor to determine how the water color is blended with the reflected and refracted world
+        WaterMaterial.waveLength = 0.1; // The lenght of waves. With smaller values, more waves are generated
     })();
     var water = BABYLON.Mesh.CreateGround("water", 1000, 1000, 1, scene, false);
 
