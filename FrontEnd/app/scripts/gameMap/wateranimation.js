bcc2f3646a7b114b4c8b75eb949d6331f52b57c7
NablaT <remi.pourtier@laposte.net>
2016-01-08 20:16:53 +0100

    Updating hole texture and magma fountain

--- FrontEnd/app/scripts/gameMap/wateranimation.js
+++ FrontEnd/app/scripts/gameMap/wateranimation.js
@@ -11,9 +11,8 @@ function createScene() {
   var canvas = document.getElementById("renderCanvas");
   var engine = new BABYLON.Engine(canvas, true);
   var scene = new BABYLON.Scene(engine);
+
   //Initialisation camera
-  //var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, BABYLON.Vector3.Zero(), scene);
-  //var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10,new BABYLON.Vector3(0, 0, 0), scene);
   var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 20, new BABYLON.Vector3(0, 0, 0), scene);
 
   var sun = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(60, 100, 10), scene);
@@ -107,9 +106,14 @@ function initialisationLights(scene) {
  */
 function createGolfHole(scene) {
   var hole = BABYLON.Mesh.CreateCylinder("cylinder", 3, 30, 10, 6, 1, scene, false);// 15,15,
+  var holeMaterial = new BABYLON.StandardMaterial("cylinder", scene);
   hole.position = new BABYLON.Vector3(0, 6, -350);
   hole.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0);
   hole.diffuseColor = new BABYLON.Color3(0, 0, 0);
+
+
+  holeMaterial.diffuseTexture = new BABYLON.Texture("textures/hole.png", scene);
+  hole.material=holeMaterial;
   return hole;
 }
 
@@ -119,7 +123,7 @@ function createGolfHole(scene) {
  */
 function initialisationSkyBox(scene) {
   // Skybox
-  var skybox = BABYLON.Mesh.CreateBox("skyBox", 1900.0, scene);
+  var skybox = BABYLON.Mesh.CreateBox("skyBox", 2000.0, scene);
   var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
   skyboxMaterial.backFaceCulling = false;
   skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("skybox/TropicalSunnyDay", scene);
@@ -156,7 +160,7 @@ function initialisationFountain(scene) {
   // On crée ensuite l'objet fountain. Cet objet est une sphere et va servir de "source visuelle"
   // a la fontaine.
   var fountain = BABYLON.Mesh.CreateSphere("foutain", 1, 1, scene); // 15,15,
-  fountain.position = new BABYLON.Vector3(250, 17, 350);
+  fountain.position = new BABYLON.Vector3(250, 17, 550);
   fountain.rotation = new BABYLON.Vector3(Math.PI / 2, Math.PI, 2 * (Math.PI));
   return fountain;
 }
@@ -167,25 +171,24 @@ function initialisationFountain(scene) {
  * @param fountain
  * @returns {BABYLON.ParticleSystem}
  */
-
 function initialisationParticles(scene, fountain) {
-
   // On cree une particule. Cette particule correspond a une goute d'eau de la fontaine.
-  var particleSystem = new BABYLON.ParticleSystem("particles", 2000, scene);
+  var particleSystem = new BABYLON.ParticleSystem("particles", 10000, scene);
 
   //On definit la texture de la particule
   particleSystem.particleTexture = new BABYLON.Texture("textures/flare.png", scene);
 
-  // On definit la source de la particule. L'emeteur est ici notre fontaine.
+  // On definit la source de la particule. L'emetteur est ici notre fontaine.
   particleSystem.emitter = fountain; // the starting object, the emitter
-  particleSystem.minEmitBox = new BABYLON.Vector3(20, 0, 0); // Starting all from
-  particleSystem.maxEmitBox = new BABYLON.Vector3(-20, 0, -1); // To...
+  particleSystem.minEmitBox = new BABYLON.Vector3(100, 0, 0); // Starting all from
+  particleSystem.maxEmitBox = new BABYLON.Vector3(-100, 0, -1); // To...
 
   // On definit les couleurs de particules. On definit un spectre de 3 couleurs.
   /*particleSystem.color1 = new BABYLON.Color4(0, 0.7, 1, 1.0);
    particleSystem.color2 = new BABYLON.Color4(0, 0.4, 1, 1.0);
    particleSystem.colorDead = new BABYLON.Color4(0, 0, 0.7, 0.0);
    */
+
   particleSystem.color1 = new BABYLON.Color4(0.7, 0, 0, 1.0);
   particleSystem.color2 = new BABYLON.Color4(1, 0, 0, 1.0);
   particleSystem.colorDead = new BABYLON.Color4(0.9, 0.3, 0.2, 0.0);
@@ -193,22 +196,22 @@ function initialisationParticles(scene, fountain) {
   // On definit la taille d'une particule. On donne deux valeurs qui sont la taille minimum
   // et la taille minimum de nos particules. Ensuite un random est fait entre ces deux valeurs
   // lors de la creation du particule
-  particleSystem.minSize = 1; //0.1;
-  particleSystem.maxSize = 1.5; //0.5;
+  particleSystem.minSize = 5; //0.1;
+  particleSystem.maxSize = 7; //0.5;
 
   // On définit la durée de vie d'une particule. Comme pour sa taille, on donne deux valeurs min/max
   //et on randomise ensuite la durée de vie d'une particule emise
-  particleSystem.minLifeTime = 0.3;
-  particleSystem.maxLifeTime = 1.5;
+  particleSystem.minLifeTime = 3.5//0.8;
+  particleSystem.maxLifeTime = 4.5;//2.1;
 
   // On definit le taux d'émission des particules
-  particleSystem.emitRate = 150000;
+  particleSystem.emitRate = 1000000;
 
   // On initialise ensuite le blend mode du system de particule à BLENDMODE_ONEONE (ou BLENDMODE_STANDARD)
   particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
 
   // On définit la gravité des particules
-  particleSystem.gravity = new BABYLON.Vector3(0, -9.81, 0);
+  particleSystem.gravity = new BABYLON.Vector3(0, 0, 0);//(0, -9.81, 0);
 
   // On définit la direction de propagation des particules
   particleSystem.direction1 = new BABYLON.Vector3(-4, 8, 3);
@@ -216,14 +219,12 @@ function initialisationParticles(scene, fountain) {
 
   // On définit l'angle des particules (aleatoirement entre le max et le min)
   particleSystem.minAngularSpeed = 0;
-  particleSystem.maxAngularSpeed = Math.PI;
+  particleSystem.maxAngularSpeed = 0;//Math.PI;
 
   // On définit la vitesse de propagation (min et max) et la vitesse de mise a jour
-  particleSystem.minEmitPower = 1;
-  particleSystem.maxEmitPower = 3;
-  particleSystem.updateSpeed = 0.005;
-
-
+  particleSystem.minEmitPower = 5;
+  particleSystem.maxEmitPower = 7;
+  particleSystem.updateSpeed = 0.015;
   return particleSystem;
 }
 
@@ -231,10 +232,8 @@ function initialisationParticles(scene, fountain) {
  * Function initialisationAnimation. This function initializes all fountain's animation.
  * @returns {*}
  */
-
 function initialisationAnimation() {
   var keys = [];
-
   //On initialise la variable animation
   var animation = new BABYLON.Animation("animation", "rotation.x", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
     BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
@@ -252,7 +251,6 @@ function initialisationAnimation() {
 
   // Launch animation
   animation.setKeys(keys);
-
   return animation;
 }
 
@@ -262,7 +260,7 @@ function initialisationAnimation() {
  */
 function initialisationWater(scene) {
   //var water = BABYLON.Mesh.CreateGround("water", 1000, 1000, 1, scene, false);
-  var waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 1600, 1600, 16, scene, false);  //2048, 2048, 16, scene, false);
+  var waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 2000, 2000, 16, scene, false);  //2048, 2048, 16, scene, false);
   var water = new BABYLON.WaterMaterial("water", scene, new BABYLON.Vector2(512, 512));
   water.backFaceCulling = true;
   water.bumpTexture = new BABYLON.Texture("textures/13_DIFFUSE.png", scene); //13_DIFFUSE
@@ -280,7 +278,6 @@ function initialisationWater(scene) {
  * Function initialisationTrack. This function create the golf track. We just create walls.
  * @param scene
  */
-
 function initialisationTrack(scene) {
   var wall1 = BABYLON.Mesh.CreateBox("wall", 10.0, scene);
   var wallMaterial1= new BABYLON.StandardMaterial("wall", scene);
