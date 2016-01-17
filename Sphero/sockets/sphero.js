972b1fe9b42c2466beb924c108dc3bad93c44a97
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-01-17 14:25:24 +0100

    Modification de la lib sphero

--- Sphero/sockets/sphero.js
+++ Sphero/sockets/sphero.js
@@ -2,34 +2,41 @@
  * Created by grahbari on 21/12/2015.
  */
 
-var sphero = require("sphero");
-var orb = sphero("/dev/tty.Sphero-BPW-AMP-SPP");
-var io = require("../core/core.js").getIO();
-
-var socket = io.connect('http://localhost:3000/sphero');
-
-socket.on('hello', function (params) {
-    console.log('hello');
-    orb.roll(100, 0);
-});
+var sphero = require("sphero"),
+    orb    = sphero("/dev/tty.Sphero-BPW-AMP-SPP"),
+    io     = require("../core/core.js").getIO(),
+    socket = io.connect('http://localhost:3000/sphero');
 
 socket.on('test', function (params) {
     test(params.dist, params.angle);
 });
+
+
 /**
- * Permet de se connecter à la sphero.
+ * Event listener.
+ * This event aims to connect the computer to the sphero. (Mac user).
  */
 socket.on('connectSphero', function (params) {
-    orb.connect(function () {
-
-    });
+    connect();
 });
 
-function test (dist, angle){
+orb.on('error', function () {
+    console.error('Sphero encountered a problem during the connection.');
+    console.info('Trying again to connect ...');
+    connect();
+});
+
+function connect () {
+    orb.connect(function () {
+        console.info('Sphero successfully connected');
+    });
+}
+
+function test (dist, angle) {
     orb.roll(dist, angle);
 }
 
-function handle(key, distance) {
+function handle (key, distance) {
 
     console.log('got "keypress"', key);
 
@@ -60,7 +67,7 @@ var router = require('../core/core.js').express.Router();
 /**
  * Cette route permet de récupérer les scores.
  */
-router.post('/key', function(req, res, next) {
+router.post('/key', function (req, res, next) {
     console.log(req.body.key);
     handle(req.body.key, req.body.distance);
     res.send('ok');
