346d75efd47223c294586bcd8a0cf194dbdbe916
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-01-20 22:43:03 +0100

    Ajout de la classe map

--- BackEnd/core/map.js
+++ BackEnd/core/map.js
@@ -14,13 +14,47 @@ class Map {
      * It creates a matrix with a value equal at 0 everywhere.
      * @param {int} width - The width of the map.
      * @param {int} height - The height of the map.
+     * @param {Position} startPosition - The start position.
+     * @param {Position} holePosition - The hole position.
+     * @param {int} rayonHole - The rayon hole.
      */
-    constructor (width, height) {
-        this._matrix = new Matrix(height, width);
+    constructor (width, height, startPosition, holePosition, rayonHole = 50) {
+        this._matrix        = new Matrix(height, width);
+        this._startPosition = startPosition;
+        this._holePosition  = holePosition;
+        this._rayonHole     = rayonHole;
     }
 
+    /**
+     * Getter of the matrix.
+     * @returns {Matrix} - The matrix.
+     */
     get matrix () {
         return this._matrix;
+    }
+
+    /**
+     * Getter of ther starter position.
+     * @returns {Position} The starter position.
+     */
+    get startPosition () {
+        return this._startPosition;
+    }
+
+    /**
+     * Getter of the hole position.
+     * @returns {Position} The hole position.
+     */
+    get holePosition () {
+        return this._holePosition;
+    }
+
+    /**
+     * Getter of the rayon hole.
+     * @returns {int} The rayon hole.
+     */
+    get rayonHole () {
+        return this._rayonHole;
     }
 
     // TODO Define this method
