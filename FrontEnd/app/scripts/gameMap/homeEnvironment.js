011d3c503cb8ea4f2490773fc2f2eaf56121002b
NablaT <remi.pourtier@laposte.net>
2016-01-22 00:52:22 +0100

    Building homepage environement (adding several objects on the map. Rotation is temporary missing

--- FrontEnd/app/scripts/gameMap/homeEnvironment.js
+++ FrontEnd/app/scripts/gameMap/homeEnvironment.js
@@ -29,9 +29,18 @@ function createScene() {
 
   //We initialize the ground
   var ground = initialisationGround(scene);
+  initialisationGround2(scene);
+  //initialisationGround3(scene);
 
+  initialisationGround3(scene);
+
+  initialisationTrack(scene);
+  initialisationMagmaSphere(scene);
+
+  initialisePodium(scene);
   //Create the golf hole
   createGolfHole(scene);
+  initialisePiquet(scene);
 
   initialisationWater(scene);
 
@@ -41,10 +50,10 @@ function createScene() {
   var alpha = 0;
   scene.registerBeforeRender(function () {
     //ground.rotation.x += 0.01;
-    ground.rotation.y += 0.001;
+   // ground.rotation.y += 0.001;
 
     //ground.position = new BABYLON.Vector3(Math.cos(alpha) * 30, 10, Math.sin(alpha) * 30);
-    alpha += 0.001;
+    //alpha += 0.001;
 
   });
   engine.runRenderLoop(function () {
@@ -96,9 +105,9 @@ function initialisationLights(scene) {
  * @returns {*}
  */
 function createGolfHole(scene) {
-  var hole = BABYLON.Mesh.CreateCylinder("cylinder", 3, 30, 10, 6, 1, scene, false);// 15,15,
+  var hole = BABYLON.Mesh.CreateCylinder("cylinder", 3, 3, 3, 6, 1, scene, false);// 15,15,
   var holeMaterial = new BABYLON.StandardMaterial("cylinder", scene);
-  hole.position = new BABYLON.Vector3(0, 6, -350);
+  hole.position = new BABYLON.Vector3(20, 6, -5);
   hole.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0);
   hole.diffuseColor = new BABYLON.Color3(0, 0, 0);
 
@@ -130,8 +139,8 @@ function initialisationSkyBox(scene) {
  * @param scene
  */
 function initialisationGround(scene) {
-  //var ground = BABYLON.Mesh.CreateGround("ground", 800, 1200, 100, scene);
-  var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "heightMaps/heightMap3.png", 90, 90, 90, 0, 10, scene, false);
+  var ground = BABYLON.Mesh.CreateGround("ground", 90, 90, 100, scene);
+  //var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "heightMaps/heightMap3.png", 90, 90, 30, 0, 10, scene, false);
 
   var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
   groundMaterial.diffuseTexture = new BABYLON.Texture("textures/grass.png", scene);
@@ -140,8 +149,125 @@ function initialisationGround(scene) {
   groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
   ground.position.y = 7;
   ground.material = groundMaterial;
+
+
   return ground;
 }
+
+/**
+ * Function initialisationGround2. This function initializes one mountain.
+ */
+function initialisationGround2(scene){
+  var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "heightMaps/heightMap.png", 30, 30, 70, 0, 10, scene, false);
+
+  var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+  groundMaterial.diffuseTexture = new BABYLON.Texture("textures/sand1.png", scene);
+  groundMaterial.diffuseTexture.uScale = 6;
+  groundMaterial.diffuseTexture.vScale = 6;
+  groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+  ground.position.z = 25;
+  ground.position.x= 24;
+  ground.position.y = 6.9;
+  ground.material = groundMaterial;
+
+
+  return ground;
+}
+
+
+/**
+ * Function initialisationGround3. This function initializes one mountain.
+ */
+function initialisationGround3(scene){
+  var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "heightMaps/heightMap3.png", 80, 30, 20, 0, 10, scene, false);
+  //hole.position = new BABYLON.Vector3(20, 6, -5);
+  var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+  groundMaterial.diffuseTexture = new BABYLON.Texture("textures/sand1.png", scene);
+  groundMaterial.diffuseTexture.uScale = 6;
+  groundMaterial.diffuseTexture.vScale = 6;
+  groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+  ground.position.z = -30;
+  ground.position.x= 0;
+  ground.position.y = 6;
+  ground.material = groundMaterial;
+
+
+  return ground;
+}
+
+/**
+ * Function initialisationGround3. This function initializes one mountain.
+ */
+function initialisationTrack(scene){
+  //var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "heightMaps/heightMap4.png", 80, 30, 5, 0, 10, scene, false);
+  var ground = BABYLON.Mesh.CreateGround("ground", 80, 15, 100, scene);
+
+  var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+  groundMaterial.diffuseTexture = new BABYLON.Texture("textures/sand3.png", scene);
+  groundMaterial.diffuseTexture.uScale = 6;
+  groundMaterial.diffuseTexture.vScale = 6;
+  groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+  ground.rotation=new BABYLON.Vector3(0, Math.PI / 5.2, 0);
+  ground.position.z = 12;
+  ground.position.x= -5;
+  ground.position.y = 7.1;
+  ground.material = groundMaterial;
+  return ground;
+
+}
+
+function initialisePodium(scene){
+  //First place
+  var cylinder = BABYLON.Mesh.CreateCylinder("cylinder",4, 4, 4, 0, 1, scene, false);
+  var cylinderMaterial = new BABYLON.StandardMaterial("cylinder", scene);
+  cylinder.position = new BABYLON.Vector3(-30, 8, -10);
+  cylinder.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0);
+  //cylinderMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+
+  cylinderMaterial.diffuseTexture = new BABYLON.Texture("textures/gold.png", scene);
+  cylinder.material = cylinderMaterial;
+
+  //Second place
+  var cylinder = BABYLON.Mesh.CreateCylinder("cylinder",2, 4, 4, 0, 1, scene, false);
+  var cylinderMaterial = new BABYLON.StandardMaterial("cylinder", scene);
+  cylinder.position = new BABYLON.Vector3(-26, 8, -10);
+  cylinder.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0);
+  //cylinderMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+
+  cylinderMaterial.diffuseTexture = new BABYLON.Texture("textures/silver.png", scene);
+  cylinder.material = cylinderMaterial;
+
+  //Third place
+  var cylinder = BABYLON.Mesh.CreateCylinder("cylinder",1, 4, 4, 0, 1, scene, false);
+  var cylinderMaterial = new BABYLON.StandardMaterial("cylinder", scene);
+  cylinder.position = new BABYLON.Vector3(-34, 7.5, -10);
+  cylinder.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0);
+  //cylinderMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+
+  cylinderMaterial.diffuseTexture = new BABYLON.Texture("textures/bronze.png", scene);
+  cylinder.material = cylinderMaterial;
+
+}
+
+function initialisePiquet(scene){
+  //First place
+  var cylinder = BABYLON.Mesh.CreateCylinder("cylinder",30, 0.7, 1, 0, 1, scene, false);
+  var cylinderMaterial = new BABYLON.StandardMaterial("cylinder", scene);
+  cylinderMaterial.diffuseColor = new BABYLON.Color3(1, 1, 1);
+  cylinder.position = new BABYLON.Vector3(20, 6, -5);
+  cylinder.material = cylinderMaterial;
+
+  var wall1 = BABYLON.Mesh.CreateBox("wall", 0.1, scene);
+  var wallMaterial1 = new BABYLON.StandardMaterial("wall", scene);
+  wall1.scaling.x = 40;
+  wall1.scaling.y = 25;
+  wall1.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0);
+  wall1.position = new BABYLON.Vector3(20, 20, -7.2);
+  wallMaterial1.diffuseTexture = new BABYLON.Texture("textures/flag.png", scene);
+  wall1.material = wallMaterial1;
+}
+
+
 
 
 /**
@@ -162,4 +288,34 @@ function initialisationWater(scene) {
   water.colorBlendFactor = 0.0;
   water.addToRenderList(skybox);
   waterMesh.material = water;
+}
+
+function initialisationMagmaSphere(scene){
+  // Box5 material
+  material = new BABYLON.StandardMaterial("bab5", scene);
+  material.emissiveColor = new BABYLON.Color3(1, 0, 0);
+  material.specularColor = new BABYLON.Color3(1, 0, 0);
+
+  //box5.material = material;
+
+  var sphere5 = BABYLON.Mesh.CreateSphere("Sphere5", 64, 5, scene);
+  sphere5.position=new BABYLON.Vector3(-50, 40, -32);
+
+  // Sphere5 material
+  material = new BABYLON.StandardMaterial("kosh5", scene);
+  material.diffuseColor = new BABYLON.Color3(0, 0, 0);
+  material.reflectionTexture = new BABYLON.CubeTexture("magma/magma", scene);
+  material.reflectionTexture.level = 3;
+  material.specularPower = 150;
+  material.emissiveColor = new BABYLON.Color3(0.05, 0.05, 0.05);
+  material.alpha = 0.8;
+
+  // Fresnel
+  material.emissiveFresnelParameters = new BABYLON.FresnelParameters();
+  material.emissiveFresnelParameters.bias = 0.4;
+  material.emissiveFresnelParameters.power = 140;
+  material.emissiveFresnelParameters.leftColor = BABYLON.Color3.Red();
+  material.emissiveFresnelParameters.rightColor = BABYLON.Color3.Blue();
+
+  sphere5.material = material;
 }
