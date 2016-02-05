ca6a9b5591891fa2eeb5ba2e06e04550f0ade87a
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-02-05 09:58:30 +0100

    Envoie des directions au screen

--- BackEnd/webAPI/kinect.js
+++ BackEnd/webAPI/kinect.js
@@ -1,12 +1,16 @@
 var router = require('../core/core.js').express.Router(),
-    kinect = require('../game/kinect.js');
+    kinect = require('../game/kinect.js'),
+    screen   = require('../sockets/screen.js');
 
 /**
  * This route aimed to update the player's direction received from kinect.
  */
 router.post('/direction', function (req, res, next) {
 
-    kinect.shootDirection = req.query['direction'];
+    var direction = req.query['direction'];
+
+    kinect.shootDirection = direction;
+    screen.emit('direction', {direction : direction});
 
     res.send('\nok\n');
 
