5b586fbd13ef6fbd67c0803d34182b3045aae575
NablaT <remi.pourtier@laposte.net>
2016-02-09 14:59:41 +0100

    refactoring

--- FrontEnd/app/scripts/controllers/homepage.js
+++ FrontEnd/app/scripts/controllers/homepage.js
@@ -68,13 +68,14 @@ angular.module('frontEndApp')
        * Function sendNumberOfPlayer. This function sends the number of player to the server and set the number of player in the service "player". It uses a function to post this number to the server.
        **/
         $scope.sendNumberOfPlayer = function () {
-          player.setNbPlayer($scope.nbOfPlayer);
+          //player.setNbPlayer($scope.nbOfPlayer);
           //services.postGameIsRunning(true);
           $scope.currentPage = "waitingFrame";
           $scope.messageForWaitingFrame = "Waiting for players ...";
           services.postNumberOfPlayer($scope.nbOfPlayer).then(
             function (data) {
               console.log(data);
+              console.log("number of players: ", $scope.nbOfPlayer);
               $scope.players = data;
               console.log("player 0", $scope.players[0]);
               //$scope.players=data
